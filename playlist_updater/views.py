2a82bccedda6f0cb7099cb81f3f583393a0dd3f3
makedozp <freefilosof@gmail.com>
2025-04-11 16:26:47 +0300

    update api

--- playlist_updater/views.py
+++ playlist_updater/views.py
@@ -1,7 +1,7 @@
+
 from django.http import FileResponse
-from django.conf import settings
 import os
 
 def serve_playlist(request):
-    file_path = os.path.join(settings.MEDIA_ROOT, 'playlist.m3u')
+    file_path = os.path.join(os.path.dirname(__file__), '../media/playlist.m3u')
     return FileResponse(open(file_path, 'rb'), content_type='text/plain')
